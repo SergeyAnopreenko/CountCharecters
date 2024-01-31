2a258a52bc2f405434e2565e7b798a61c6595d01
SergeyAnopreenko <154093984+SergeyAnopreenko@users.noreply.github.com>
2024-01-31 12:52:49 +0200

    renamed folder

---
+++ CountCharacters/Program.cs
@@ -0,0 +1,17 @@
+﻿using CountCharacters;
+
+Console.WriteLine("Enter symbols");
+
+var userInput = Console.ReadLine();
+
+CharacterCounter characterCounter = new CharacterCounter(userInput);
+
+Dictionary<char, int> sorter = characterCounter.LettersSorter();
+
+foreach (var pair in sorter)
+{
+
+    Console.WriteLine($"{pair.Key} : {pair.Value}");
+
+}
+
